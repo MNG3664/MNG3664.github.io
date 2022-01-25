a530a3209a0c4064589652246122c7dc725b7866
Simih <simi.obaado@gmail.com>
2022-01-25 08:34:28 +0100

    Updates

---
+++ precache-manifest.45e0954ef8839a338c33d1309080ac73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ebf541ff78ea9deea97d1b489d3acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427aa8e00f29b025aefb",
+    "url": "/static/css/main.bc33158a.chunk.css"
+  },
+  {
+    "revision": "e0029edde0de754e1512",
+    "url": "/static/js/2.99918fb6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.99918fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972d0236d9c8f0a2c3d8",
+    "url": "/static/js/3.a3784a4f.chunk.js"
+  },
+  {
+    "revision": "427aa8e00f29b025aefb",
+    "url": "/static/js/main.ae978be0.chunk.js"
+  },
+  {
+    "revision": "16fb0c1913e46e950f60",
+    "url": "/static/js/runtime-main.20df1c85.js"
+  }
+]);
